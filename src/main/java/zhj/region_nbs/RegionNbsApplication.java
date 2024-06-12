e0d51d037a2873bc18831760be59faeb90b73f39
zhengjun <zhengjun@agridata.org>
2024-06-12 09:04:08 +0800

    init

---
+++ src/main/java/zhj/region_nbs/RegionNbsApplication.java
@@ -0,0 +1,13 @@
+package zhj.region_nbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionNbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionNbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zhj/region_nbs/RegionNbsApplicationTests.java
@@ -0,0 +1,13 @@
+package zhj.region_nbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionNbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
